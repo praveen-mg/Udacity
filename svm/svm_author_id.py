bb4705e4d6743309c096257922fade9c867ec074
PRAVEEN KUMAR MEENAKSHI SUNDARAM GANDHI <PRAVEEN KUMAR MEENAKSHI SUNDARAM GANDHI>
2016-05-26 22:43:11 +0800

    svm processing

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,16 +12,25 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
-
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
+print "Taining Data"
+clf = SVC(kernel="rbf")
 
+print "Before Train"
+clf.fit(features_train[:len(features_train)/100],labels_train[:len(labels_train)/100])
+print "Before Prediction"
+pred = clf.predict(features_test)
 
+print "After Prediction"
 
+acc= accuracy_score(pred,labels_test)
 
+print "Accuracy value is:",acc
 #########################################################
 ### your code goes here ###
 
