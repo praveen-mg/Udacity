c23d8ca02ee7e3fe779d16769bb54bfd6a198e0e
PRAVEEN KUMAR MEENAKSHI SUNDARAM GANDHI <PRAVEEN KUMAR MEENAKSHI SUNDARAM GANDHI>
2016-05-26 23:35:39 +0800

    svm processing

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -19,17 +19,24 @@ from sklearn.metrics import accuracy_score
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 print "Taining Data"
-clf = SVC(kernel="rbf")
+clf = SVC(kernel="rbf",C=10000)
 
 print "Before Train"
-clf.fit(features_train[:len(features_train)/100],labels_train[:len(labels_train)/100])
+#clf.fit(features_train[:len(features_train)/100],labels_train[:len(labels_train)/100])
+clf.fit(features_train,labels_train)
 print "Before Prediction"
 pred = clf.predict(features_test)
 
 print "After Prediction"
-
+print "Predict Value for 10:",pred[10]
+print "Predict Value for 26:",pred[26]
+print "Predict Value for 50:",pred[50]
 acc= accuracy_score(pred,labels_test)
-
+count=0
+for val in pred:
+    if val ==1:
+        count = count+1
+print "Total Chris:", count
 print "Accuracy value is:",acc
 #########################################################
 ### your code goes here ###
