db196d980b783459bf0a91ff7fc720cbc2e1efcb
PRAVEEN KUMAR MEENAKSHI SUNDARAM GANDHI <PRAVEEN KUMAR MEENAKSHI SUNDARAM GANDHI>
2016-06-20 09:15:02 +0800

    K mean and minmax

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -16,7 +16,7 @@ sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
 from sklearn.cluster import KMeans
-
+from sklearn import preprocessing
 
 def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature 1", f2_name="feature 2"):
     """ some plotting code designed to help you visualize your clusters """
@@ -51,19 +51,27 @@ value_stock = []
 value_salary = []
 for key,val in data_dict.iteritems():
     if (not math.isnan(float(val['exercised_stock_options']))):
-       
-        value_stock.append(val['exercised_stock_options'])
+        
+        value_stock.append(float(val['exercised_stock_options']))
     if (not math.isnan(float(val['salary']))):
-        value_salary.append(val['salary'])
+        value_salary.append(float(val['salary']))
+        if val['salary'] == 200000:
+            print "Value is available"
 #print value
 print "Minimum stock Value:",min(value_stock)
 print "Maximum stock Value:",max(value_stock)
 
 print "Minimum salary:", min(value_salary)
 print "Maximum salary:", max(value_salary)
-
-
-
+value_array_stock  = numpy.array([min(value_stock),1000000.,max(value_stock)])
+value_array_salary = numpy.array([min(value_salary),200000.,max(value_salary)])
+#value_salary_array = numpy.array([])
+#print "Arg of Salary:",value_salary_array.argwhere(200000)
+scaler = preprocessing.MinMaxScaler()
+train_minmax_stock = scaler.fit_transform(value_array_stock)
+print train_minmax
+train_minmax_salary = scaler.fit_transform(value_array_salary)
+print train_minmax_salary 
 k_means = KMeans(n_clusters=2)
 
 #k_means.fit(features)
@@ -74,18 +82,29 @@ feature_1 = "salary"
 feature_2 = "exercised_stock_options"
 #feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2]
+features_list = [poi,feature_1, feature_2]
 data = featureFormat(data_dict, features_list )
+#print data
 poi, finance_features = targetFeatureSplit( data )
+#print finance_features
 k_means.fit(finance_features)
 pred = k_means.predict(finance_features)
 ### in the "clustering with 3 features" part of the mini-project,
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
+#print data_dict
 for f1, f2 in finance_features:
     plt.scatter( f1, f2)
 plt.show()
+scaler = preprocessing.MinMaxScaler()
+
+    
+#salary = numpy.array(salary)
+#stock  = numpy.array(stock)
+
+    
+#train_minmax = scaler.fit_transform(salary,stock)
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
